173f7f6f1d0e6e2532bc406a10aaac971c7ed732
Vignesh Venugopal <vickyv@live.in>
2017-06-09 12:31:29 +0530

    Adding comments to the code

--- functions/sentimentAnalysis.R
+++ functions/sentimentAnalysis.R
@@ -2,9 +2,10 @@ sentimentAnalysis <- function(wordList){
   
   sentenceVector <- get_sentences(wordList)
   nrcData <- get_nrc_sentiment(sentenceVector)
-  sentiment <- pander::pandoc.table(nrcData, split.table = Inf)
+  pander::pandoc.table(nrcData, split.table = Inf)
   
-  emotionPlot <- barplot(
+  par(mfrow= c(2, 1))
+  barplot(
     sort(colSums(prop.table(nrcData[, 1:8]))), 
     horiz = TRUE, 
     cex.names = 0.7, 
@@ -12,14 +13,11 @@ sentimentAnalysis <- function(wordList){
     main = "Emotions in Data", xlab="Percentage"
   )
   
-  polarityPlot <- barplot(
+  barplot(
     sort(colSums(prop.table(nrcData[,9:10]))), 
     horiz = TRUE, 
     cex.names = 0.7, 
     las = 1, 
     main = "Polarity in Data", xlab="Percentage"
   )
-  
-  return(list(emotionPlot, polarityPlot))
-  
 }
--- processData/generateInsights.R
+++ processData/generateInsights.R
@@ -1,5 +1,3 @@
-# required pakacges
-
 library(tm)
 library(qdap)
 library(openNLP)
@@ -14,6 +12,10 @@ source("https://raw.githubusercontent.com/vigneshvenugopal/socialMediaAdEffectiv
 source("https://raw.githubusercontent.com/vigneshvenugopal/socialMediaAdEffectiveness/master/functions/sentimentAnalysis.R")
 source("https://raw.githubusercontent.com/vigneshvenugopal/socialMediaAdEffectiveness/master/functions/topicModelling.R")
 
+###################
+# Loading the data
+###################
+
 twitterData <- read.csv("RivaahBridesByTanishq_twitter_Tweets.csv")
 tweets <- as.data.frame(twitterData$text)
 tweets$source<-1
@@ -26,35 +28,50 @@ youTubeData <- read.csv("RivaahBridesByTanishq_YouTube_Video_Comments.csv")
 youTubeComments <- as.data.frame(youTubeData$text)
 youTubeComments$source<-3
 colnames(youTubeComments)[1] <- "text"
-finalData <- rbind(tweets, fbComments, youTubeComments)
+completeData <- rbind(tweets, fbComments, youTubeComments)
 
-myStopWords<- c(stopwords("SMART"),"http","https","bitly","sticke","www","pictwittercom","bhi","pictwitter","com","shavetjain","youtube","pakistan","zeevekadlm","ghaywan","singh","sharma","such","sticker")
-wordFrequency = 10
-correlationValue = 0.1
-associationWord = "emotional"
-associationValue = 0.1
-wordCloudMaxWords = 100
-wordCloudMinFreq = 7
-corrWord = 'beautiful'
 
+##############################
+# Setting up the input params
+##############################
+
+# Setup the below mentioned params for each datasource (i.e., completeData, Facebook, Twitter & YouTube),
+# And execute the data processing code below available below
+inputData <- completeData$text
+myStopWords <- c(stopwords("SMART"),"http","https","bitly","sticke","www","pictwittercom","bhi","pictwitter","com","shavetjain","youtube","pakistan","zeevekadlm","ghaywan","singh","sharma","such","sticker")
+wordFrequency <- 10
+correlationValue <- 0.1
+associationWord <- "emotional"
+associationValue <- 0.1
+wordCloudMaxWords <- 100
+wordCloudMinFreq <- 7
+corrWord <- 'beautiful'
+
+#####################################
+# Process data and generate insights
+#####################################
+
+# Cleaning the Data
 corpusList <- cleanSocialMediaText(finalData$text, myStopWords)
 myTmCorpus <- corpusList[[1]]
 myCorpus <- corpusList[[2]]
 
+# POS Tagging
 posDataReturnList <- posTagging(myTmCorpus, myCorpus)
 posTaggedData <- posDataReturnList[[1]]
 myWordList <- posDataReturnList[[2]]
 myTmCorpusWordList <- posDataReturnList[[3]]
 myVCorpus <- posDataReturnList[[4]]
 
+# Plotting metrics for the text
 plotReturnList <- plotTextMetrics(myTmCorpusWordList, myVCorpus, wordFrequency, correlationValue, associationWord, associationValue, wordCloudMaxWords, wordCloudMinFreq, corrWord)
 plotReturnList[[1]]
 plotReturnList[[2]]
 plotReturnList[[3]]
 plotReturnList[[4]]
 
-sentiPlotList <- sentimentAnalysis(myWordList)
-sentiPlotList[[1]]
-sentiPlotList[[2]]
+#Sentiment Analysis
+sentimentAnalysis(myWordList)
 
+#Topics Modelling
 topicModelling(posTaggedData, myTmCorpusWordList)
