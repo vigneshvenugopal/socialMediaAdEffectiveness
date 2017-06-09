ad82696c5c85be1e1cc986e15bf42025c360fada
Vignesh Venugopal <vickyv@live.in>
2017-06-09 09:04:34 +0530

    Returning the sentiment plots

--- functions/sentimentAnalysis.R
+++ functions/sentimentAnalysis.R
@@ -4,7 +4,7 @@ sentimentAnalysis <- function(wordList){
   nrcData <- get_nrc_sentiment(sentenceVector)
   sentiment <- pander::pandoc.table(nrcData, split.table = Inf)
   
-  barplot(
+  emotionPlot <- barplot(
     sort(colSums(prop.table(nrcData[, 1:8]))), 
     horiz = TRUE, 
     cex.names = 0.7, 
@@ -12,12 +12,14 @@ sentimentAnalysis <- function(wordList){
     main = "Emotions in Data", xlab="Percentage"
   )
   
-  barplot(
+  polarityPlot <- barplot(
     sort(colSums(prop.table(nrcData[,9:10]))), 
     horiz = TRUE, 
     cex.names = 0.7, 
     las = 1, 
     main = "Polarity in Data", xlab="Percentage"
   )
+  
+  return(list(emotionPlot, polarityPlot))
   
 }
